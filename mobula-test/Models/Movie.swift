04482eddb3301cc2e3eda8e706d109b4b3e49dc1
Tancrède Chazallet (AncAinu) <contact@ancainu.com>
2017-07-12 11:48:12 +0100

    Parsing more info in movie model

--- mobula-test/Models/Movie.swift
+++ mobula-test/Models/Movie.swift
@@ -9,9 +9,25 @@
 import Foundation
 
 class Movie {
+	var id: Int
 	var title: String
+	var description: String
+	var director: String
+	var starring: String?
+	var thumbnailUrl: URL?
+	var trailerCoverUrl: URL?
+	var ticketUrl: URL?
+	var trailerYoutubeId: String?
 	
 	init(info: [String: Any]) {
+		self.id = info["id"] as? Int ?? 0
 		self.title = info["title"] as? String ?? ""
+		self.description = info["description"] as? String ?? ""
+		self.director = info["director"] as? String ?? ""
+		self.starring = info["starring"] as? String ?? ""
+		self.thumbnailUrl = URL(string: info["thumb_url"] as? String ?? "")
+		self.trailerCoverUrl = URL(string: info["trailer_cover_url"] as? String ?? "")
+		self.ticketUrl = URL(string: info["ticket_url"] as? String ?? "")
+		self.trailerYoutubeId = info["trailer_youtube_id"] as? String ?? ""
 	}
 }
